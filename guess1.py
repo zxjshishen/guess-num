af56b8acc020d7eed1d9cd57fcd16733427ff56f
zxjshishen <zxjshishen@163.com>
2019-05-29 15:39:11 +0800

    basic version

---
+++ guess1.py
@@ -0,0 +1,12 @@
+import random
+r = random.randint(1, 100)
+while True:
+	num = input('请输入你所猜的数字： ')
+	num = int(num)
+	if num == r:
+		print('你猜中了！')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
